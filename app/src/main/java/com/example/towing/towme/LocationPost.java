d45ffa8c953c2b482632767a83ac8c2e66020cd4
Mohamed Abo El Soud <mohamed.aaes@gmail.com>
2014-11-29 12:59:22 -0500

    parse application testing

---
+++ app/src/main/java/com/example/towing/towme/LocationPost.java
@@ -0,0 +1,43 @@
+package com.example.towing.towme;
+
+import com.parse.ParseClassName;
+import com.parse.ParseGeoPoint;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+/**
+ * Created by Mohamed on 14-11-29.
+ */
+
+@ParseClassName("Posts")
+public class LocationPost extends ParseObject {
+
+    public String getText() {
+        return getString("text");
+    }
+
+    public void setText(String value) {
+        put("text", value);
+    }
+
+    public ParseUser getUser() {
+        return getParseUser("user");
+    }
+
+    public void setUser(ParseUser value) {
+        put("user", value);
+    }
+
+    public ParseGeoPoint getLocation() {
+        return getParseGeoPoint("location");
+    }
+
+    public void setLocation(ParseGeoPoint value) {
+        put("location", value);
+    }
+
+    public static ParseQuery<LocationPost> getQuery() {
+        return ParseQuery.getQuery(LocationPost.class);
+    }
+}
