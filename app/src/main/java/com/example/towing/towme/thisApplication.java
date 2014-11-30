b88bb604962175f6edfea1efa3be227a1bc515bb
Mohamed Abo El Soud <mohamed.aaes@gmail.com>
2014-11-30 18:14:44 -0500

    minor ui fixes

---
+++ app/src/main/java/com/example/towing/towme/thisApplication.java
@@ -0,0 +1,19 @@
+package com.example.towing.towme;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+/**
+ * Created by Mohamed on 14-11-30.
+ */
+public class ThisApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ParseObject.registerSubclass(LocationPost.class);
+        Parse.initialize(this, getString(R.string.parse_application_id)
+                , getString(R.string.parse_client_key));
+    }
+}
